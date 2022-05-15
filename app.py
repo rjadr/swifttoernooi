edcbd7af971095e301ff6d79f48a29473b678deb
rjadr <ouwedibbes@hotmail.com>
2022-05-15 21:56:10 +0200

    Initial commit

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 from streamlit_cookies_manager import EncryptedCookieManager
+from shapely.geometry import Point
+
 st.set_page_config(page_title='Swift Hemelvaartsdagtoernooi', page_icon="favicon.ico", layout="wide")
 # This should be on top of your script
 cookies = EncryptedCookieManager(
@@ -62,7 +64,6 @@ stand, schema = get_data()
 ################
 
 logoswift = Image.open('swiftlogo.png')
-logojumbo = Image.open('jumbologo.jpg')
 plattegrond = Image.open('plattegrond_hemelvaart.jpg')
 
 if choose == "Welkom":
@@ -75,12 +76,7 @@ if choose == "Welkom":
     with col2:  # To display brand log
         st.image(logoswift, width=130)
 
-    st.markdown("Het is weer zo ver, voor de 72e keer in de geschiedenis van Swift vindt het Jumbo-Swift Hemelvaartsdagtoernooi plaats! Op het moment van het schrijven van dit voorwoord hebben 14 verenigingen en 92 teams aangemeld. Heel erg fijn dat zoveel sporters naar Swift komen om te genieten van dit jaarlijkse toernooi!\n\nHet toernooi wordt mede mogelijk gemaakt door onze sponsor [Jumbo Oostkapelle/Middelburg](https://www.jumbo.com/).\n\n")
-
-    col1, col2, col3 = st.columns([1.4, 3, 1.4])
-    col2.image(logojumbo, use_column_width=True)
-
-    st.markdown("Op deze site vindt u alle informatie over het toernooi. U kunt o.a. het wedstrijdschema, de standen, de plattegrond en het wedstrijdreglement bekijken. Browst u mobiel? Klik dan op de pijl linksboven om het menu te zien.\n\nWe wensen u een mooi toernooi toe!\n\nVriendelijke groet,\n\nDe toernooicommissie van het Jumbo-Swift Hemelvaartsdagtoernooi")
+    st.markdown("Het is weer zo ver, voor de 72e keer in de geschiedenis van Swift vindt het kv Swift Hemelvaartsdagtoernooi plaats! Op het moment van het schrijven van dit voorwoord hebben 14 verenigingen en 92 teams aangemeld. Heel erg fijn dat zoveel sporters naar Swift komen om te genieten van dit jaarlijkse toernooi!\n\nOp deze site vindt u alle informatie over het toernooi. U kunt o.a. het wedstrijdschema, de standen, de plattegrond en het wedstrijdreglement bekijken. Browst u mobiel? Klik dan op de pijl linksboven om het menu te zien.\n\nWe wensen u een mooi toernooi toe!\n\nVriendelijke groet,\n\nDe toernooicommissie van het kv Swift Hemelvaartsdagtoernooi")
 
 if choose == "Toernooi-informatie":
     st.markdown(""" <style> .font {
@@ -88,7 +84,50 @@ if choose == "Toernooi-informatie":
     </style> """, unsafe_allow_html=True)
     st.markdown('<p class="font">Toernooi-informatie</p>', unsafe_allow_html=True)
 
-    st.markdown("Hieronder vindt u praktische informatie over de toernooidag:\n\n### Parkeren\n\nParkeren met de auto is mogelijk op het parkeerterrein van Sportpark de Sprong. Fietsers verzoeken wij de fiets te parkeren in de fietsenrekken voor de ingang van de sporthal en niet bij (de toegangsweg naar) het clubhuis.\nDe toegangsweg naar het complex is niet voor autoverkeer toegankelijk en dient vrij gehouden te worden voor de hulpdiensten.\n\n### Aankomst/melden\n\nWij verzoeken teams dringend op tijd aanwezig te zijn en zich uiterlijk om 10:00 uur te melden bij de wedstrijdleiding (bij het materialenhok naast het clubhuis, zie plattegrond). We wijzen erop dat het niet mogelijk is een wedstrijd op een later tijdstip te spelen. Zie punt 4 van het wedstrijdreglement.\n\nDe terreinen zijn toegankelijk vanaf 9 uur 's morgens.\n\n### Warming up\n\n....\n\n### Kinderactiviteiten\n\n....\n\n### Afval\n\nVoor afval treft u zowel bij de kleedruimten, kantine als op het veld speciale afvalbakken aan. Men wordt dringend verzorgd deze te gebruiken!\n\n### EHBO\n\nDe EHBO-post bevindt zich bij de kleedkamers naast het clubhuis.\n\n### Prijzen\n\nVoor elke afdeling is één prijs beschikbaar. De prijsuitreiking vindt plaats om ??? uur bij ???. Voor de D,E en F-jeugd is er voor ieder kind een vaantje beschikbaar, deze kunnen vanaf 12.00 uur door de coaches opgehaald worden bij de wedstrijdleiding.\n\n### (Kunst)grasvelden\n\nVelden S4 (senioren), E1, E2, E3, F1 en F2 zijn gelegen op de kunstgrasvelden. Alle andere velden op natuurgras. Zie het wedstrijdschema om te kijken op welke velden je speelt.\n\n### Fluiten\n\nNa aankomst ontvangt u de wedstrijdbriefjes met de te fluiten wedstrijden. Wilt u de uitslagen z.s.m. na afloop van de wedstrijd doorgeven bij de wedstrijdleiding? De scheidsrechters geven de bal door aan de volgende scheidsrechter, tenzij op het briefje verzocht wordt de bal bij de leiding in te leveren.\n\n### Overig\n\nK.v. Swift stelt zich niet aansprakelijk voor het zoekraken of beschadigen van eigendommen. Het is de deelnemende verenigingen niet toegestaan tijdens het toernooi verkoopacties te houden op de velden.\n\n###Eten en drinken\n\nEten en drinken wordt afgerekend met consumptiebonnen die op verschillende punten te verkrijgen zijn.\n\n### Locatie en contact\n\nKorfbalvereniging Swift\n\nSportcomplex de Sprong\n\nDe Aanloop 5\n\n4335 AT Middelburg\n\nTel. 0118 850 437 (clubhuis)\n\ne-mail: [kvswifthemelvaart@gmail.com](mailto:kvswifthemelvaart@gmail.com)")
+
+    st.markdown("Hieronder vindt u praktische informatie over de toernooidag:\n\n### Programma")
+    table = {'Tijd': {0: '10:00',
+                    1: '10:30',
+                    2: '11:00',
+                    3: '11:30',
+                    4: '12:00',
+                    5: '12:30',
+                    6: '13:00',
+                    7: '13:30',
+                    8: '14:00',
+                    9: '14:30',
+                    10: '15:00',
+                    11: '15:30',
+                    12: '16:10'},
+     'Kinderprogramma (Kangoeroes, F, E, D)': {0: 'Opening toernooi met warming up op hoofdveld Swift',
+                                               1: 'Schminken, Tekenen, Springkussen',
+                                               2: 'Schminken, Tekenen, Springkussen',
+                                               3: 'Schminken, Tekenen, Springkussen',
+                                               4: 'Springkussen',
+                                               5: 'Springkussen',
+                                               6: 'Spelletjesparcours, Springkussen',
+                                               7: 'Spelletjesparcours, Springkussen',
+                                               8: 'Spelletjesparcours, Springkussen',
+                                               9: 'Spelletjesparcours, Springkussen',
+                                               10: 'Springkussen',
+                                               11: 'Springkussen',
+                                               12: ' '},
+     'Toernooi': {0: 'Opening toernooi met warming up op hoofdveld Swift',
+                  1: 'Ronde 1',
+                  2: 'Ronde 2',
+                  3: 'Ronde 3',
+                  4: 'Ronde 4',
+                  5: 'Ronde 5',
+                  6: 'Ronde 6',
+                  7: 'Ronde 7',
+                  8: 'Ronde 8',
+                  9: 'Ronde 9',
+                  10: 'Ronde 10',
+                  11: 'Ronde 11',
+                  12: 'Prijsuitreiking'}}
+
+    st.table(pd.DataFrame(table))
+    st.markdown("### Parkeren\n\nParkeren met de auto is mogelijk op het parkeerterrein van Sportpark de Sprong. Fietsers verzoeken wij de fiets te parkeren in de fietsenrekken voor de ingang van de sporthal en niet bij (de toegangsweg naar) het clubhuis.\nDe toegangsweg naar het complex is niet voor autoverkeer toegankelijk en dient vrij gehouden te worden voor de hulpdiensten.\n\n### Aankomst/melden\n\nWij verzoeken teams dringend op tijd aanwezig te zijn en zich uiterlijk om 10:00 uur te melden bij de wedstrijdleiding (bij het materialenhok naast het clubhuis, zie plattegrond). We wijzen erop dat het niet mogelijk is een wedstrijd op een later tijdstip te spelen. Zie punt 4 van het wedstrijdreglement.\n\nDe terreinen zijn toegankelijk vanaf 9 uur 's morgens.\n\n### Kinderactiviteiten\n\nNaast de wedstrijden dit jaar extra activiteiten voor de jeugd. Het toernooi zal geopend worden met een muzikale warming-up waarbij iedereen zich klaar kan maken voor een sportieve dag. Alle spelende en niet-spelende jeugd kan meedoen, maar ook senioren die een extra warming-up nodig hebben zijn meer dan welkom om aan te sluiten. Verder kun je je dit jaar laten schminken en een sprongetje wagen op het springkussen. In de middag vinden er spelletjes plaats voor de jonge jeugd. Voltooi alle spellen en krijg een kleine beloning! Neem je broertjes, zusjes, vriendjes en vriendinnetjes mee om samen tussen de wedstrijden door te spelen.\n\nHiernaast is het spel Turf War te spelen via [https://hemelvaart.kvswift.nl](https://hemelvaart.kvswift.nl). Wie verovert Sportpark de Sprong namens zijn club? Check met je mobiel in op zoveel mogelijk locaties op het sportpark. De club die het langst zoveel mogelijk locaties in bezit houdt wint de Turf War!\n\n### Afval\n\nVoor afval treft u zowel bij de kleedruimten, kantine als op het veld speciale afvalbakken aan. Men wordt dringend verzorgd deze te gebruiken!\n\n### EHBO\n\nDe EHBO-post bevindt zich bij de kleedkamers naast het clubhuis.\n\n### Prijzen\n\nVoor elke afdeling is één prijs beschikbaar. De prijsuitreiking vindt plaats om ??? uur bij ???. Voor de D,E en F-jeugd is er voor ieder kind een vaantje beschikbaar, deze kunnen vanaf 12.00 uur door de coaches opgehaald worden bij de wedstrijdleiding.\n\n### (Kunst)grasvelden\n\nVelden S4 (senioren), E1, E2, E3, F1 en F2 zijn gelegen op de kunstgrasvelden. Alle andere velden op natuurgras. Zie het wedstrijdschema om te kijken op welke velden je speelt.\n\n### Fluiten\n\nNa aankomst ontvangt u de wedstrijdbriefjes met de te fluiten wedstrijden. Wilt u de uitslagen z.s.m. na afloop van de wedstrijd doorgeven bij de wedstrijdleiding? De scheidsrechters geven de bal door aan de volgende scheidsrechter, tenzij op het briefje verzocht wordt de bal bij de leiding in te leveren.\n\n### Overig\n\nK.v. Swift stelt zich niet aansprakelijk voor het zoekraken of beschadigen van eigendommen. Het is de deelnemende verenigingen niet toegestaan tijdens het toernooi verkoopacties te houden op de velden.\n\n### Eten en drinken\n\nEten en drinken wordt afgerekend met consumptiebonnen die in het clubhuis te verkrijgen zijn. 's Middags zijn ze ook te koop bij het snoep/ijs.\n\n### Locatie en contact\n\nKorfbalvereniging Swift\n\nSportcomplex de Sprong\n\nDe Aanloop 5\n\n4335 AT Middelburg\n\nTel. 0118 850 437 (clubhuis)\n\ne-mail: [kvswifthemelvaart@gmail.com](mailto:kvswifthemelvaart@gmail.com)")
     st.map(data=pd.DataFrame([{'lat':51.497489353450895, 'lon':3.594653606414795}]))
 
 if choose == "Wedstrijdschema":
@@ -223,82 +262,96 @@ if choose == "Wedstrijdreglement":
 if choose == "Turf War":
     import leafmap.foliumap as leafmap
 
-    if not cookies.ready():
-        # Wait for the component to load and send us current cookies.
+    st.markdown('Under construction')
+    password = st.text_input("Enter password", type="password")
+    if not password == "ffwachten":
         st.stop()
-
-    lat_location = False
-    lon_location = False
-    result = False
-
-    st.markdown(""" <style> .font {
-    font-size:35px ; font-family: 'Cooper Black'; color: #FF9900;} 
-    </style> """, unsafe_allow_html=True)
-    if 'club' in cookies:
-        title = 'Turf War - ' + cookies['club']
     else:
-        title = 'Turf War'
-    st.markdown(f'<p class="font">{title}</p>', unsafe_allow_html=True)
 
-   #st.write("Current cookies:", cookies)
+        if not cookies.ready():
+            # Wait for the component to load and send us current cookies.
+            st.stop()
 
-    if not 'club' in cookies:
-        teams = sorted(
-            [i for i in np.unique(schema[['Thuis', 'Uit']]) if not i.startswith('No ') and not i.startswith('No.')])
-        verenigingen = sorted(set([j[0] if (j := i.split(' ')) and len(str(j[-1])) <= 2 else i for i in teams]))
+        lat_location = False
+        lon_location = False
+        result = False
 
-        option = st.selectbox('Kies je club:', ['']+verenigingen,
-                                format_func=lambda x: 'Kies een club' if x == '' else x)
+        st.markdown(""" <style> .font {
+        font-size:35px ; font-family: 'Cooper Black'; color: #FF9900;} 
+        </style> """, unsafe_allow_html=True)
+        if 'club' in cookies:
+            title = 'Turf War - ' + cookies['club']
+        else:
+            title = 'Turf War'
+        st.markdown(f'<p class="font">{title}</p>', unsafe_allow_html=True)
 
-        if option:
+       #st.write("Current cookies:", cookies)
 
-            st.warning(f'Je hebt {option} geselecteerd. Bevestig je keuze of kies een andere club. Je kunt je club tijdens het spel niet meer wijzigen.')
+        if not 'club' in cookies:
+            teams = sorted(
+                [i for i in np.unique(schema[['Thuis', 'Uit']]) if not i.startswith('No ') and not i.startswith('No.')])
+            verenigingen = sorted(set([j[0] if (j := i.split(' ')) and len(str(j[-1])) <= 2 else i for i in teams]))
 
-            if st.button("Bevestig"):
-                with st.spinner(f"Registreren voor {option}"):
-                    cookies['club'] = option  # This will get saved on next rerun
-                    cookies.save()  # Force saving the cookies now, without a rerun
-                    st.success(f'Je speelt mee namens {option}')
-                    st.experimental_rerun()
+            option = st.selectbox('Kies je club:', ['']+verenigingen,
+                                    format_func=lambda x: 'Kies een club' if x == '' else x)
 
-    if 'club' in cookies:
-        loc_button = Button(label="Get Device Location", max_width=150)
-        loc_button.js_on_event(
-            "button_click",
-            CustomJS(
-                code="""
-            navigator.geolocation.getCurrentPosition(
-                (loc) => {
-                    document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
-                }
+            if option:
+
+                st.warning(f'Je hebt {option} geselecteerd. Bevestig je keuze of kies een andere club. Je kunt je club tijdens het spel niet meer wijzigen.')
+
+                if st.button("Bevestig"):
+                    with st.spinner(f"Registreren voor {option}"):
+                        cookies['club'] = option  # This will get saved on next rerun
+                        cookies.save()  # Force saving the cookies now, without a rerun
+                        st.success(f'Je speelt mee namens {option}')
+                        st.experimental_rerun()
+
+        if 'club' in cookies:
+            loc_button = Button(label="Get Device Location", max_width=150)
+            loc_button.js_on_event(
+                "button_click",
+                CustomJS(
+                    code="""
+                navigator.geolocation.getCurrentPosition(
+                    (loc) => {
+                        document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
+                    }
+                )
+                """
+                ),
             )
-            """
-            ),
-        )
-        result = streamlit_bokeh_events(
-            loc_button,
-            events="GET_LOCATION",
-            key="get_location",
-            refresh_on_update=False,
-            override_height=75,
-            debounce_time=0,
-        )
+            result = streamlit_bokeh_events(
+                loc_button,
+                events="GET_LOCATION",
+                key="get_location",
+                refresh_on_update=False,
+                override_height=75,
+                debounce_time=0,
+            )
 
-    if result:
-        if "GET_LOCATION" in result:
-            loc = result.get("GET_LOCATION")
-            lat_location = loc.get("lat")
-            lon_location = loc.get("lon")
-            st.success(f"Lat, Lon: {lat_location}, {lon_location}")
+        file_path = 'Hemelvaart_De_Sprong.geojson'
+        gdf = gpd.read_file(file_path)
 
-    file_path = 'Hemelvaart_De_Sprong.geojson'
-    gdf = gpd.read_file(file_path)
-    lon, lat = leafmap.gdf_centroid(gdf)
+        #test
+        result= {'GET_LOCATION': True,'lat': 51.497130, 'lon': 3.593360}
+        if result:
+            if "GET_LOCATION" in result:
+                #loc = result.get("GET_LOCATION")
+                #lat_location = float(loc.get("lat"))
+                #lon_location = float(loc.get("lon"))
+                lat_location = float(result["lat"])
+                lon_location = float(result["lon"])
+                #filter gdf on geometries that contain the location Point
+                hits = gdf[gdf['geometry'].contains(Point(lon_location, lat_location))].iloc[0]
+                if len(hits) > 0:
+                    st.success(f"Lat, Lon: {lat_location}, {lon_location}")
 
-    m = leafmap.Map(center=(lat, lon), draw_export=False, draw_control=False, measure_control=False,
-                    fullscreen_control=False, attribution_control=True)
-    m.add_gdf(gdf, layer_name='Turf Wars Hemelvaart')
-    if lat_location and lon_location:
-        m.add_marker(location=(lat_location, lon_location))
-    m.zoom_to_gdf(gdf)
-    m.to_streamlit(add_layer_control=True)
+        lon, lat = leafmap.gdf_centroid(gdf)
+
+        m = leafmap.Map(center=(lat, lon), draw_export=False, draw_control=False, measure_control=False,
+                        fullscreen_control=False, attribution_control=True)
+        m.add_gdf(gdf, layer_name='Turf Wars Hemelvaart')
+        if lat_location and lon_location:
+            m.add_marker(location=(lat_location, lon_location))
+        m.zoom_to_gdf(gdf)
+        m.to_streamlit(add_layer_control=True)
