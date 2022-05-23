b8622e94b5005ecb6257d9fc92737e4a21e9fadd
RJ Adriaansen <ouwedibbes@hotmail.com>
2022-05-23 22:19:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -546,12 +546,10 @@ components.html("""
 <script type="text/javascript">
 const doc = window.parent.document.querySelector('[title="streamlit_option_menu.option_menu"]');
 const links = doc.contentWindow.document.querySelectorAll(".nav-link");
-const buttons = window.parent.document.getElementsByClassName("css-ex7byz edgvbvh6");
-const buttons2 = window.parent.document.querySelector('section[data-testid="stSidebar"]').getElementsByTagName('button');
-console.log(buttons2);
+const buttons = window.parent.document.querySelector('section[data-testid="stSidebar"]').getElementsByTagName('button');
 for (const link of links) {
     link.addEventListener('click', function() {
-    buttons[1].click();
+    buttons[0].click();
     return false;
   });
 }
